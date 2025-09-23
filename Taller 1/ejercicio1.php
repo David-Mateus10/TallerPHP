66a8890410f562cb3570ce75ce91ac22d1cba75c
Joel-Baez <joebaez@uniboyaca.edu.co>
2025-09-23 15:44:16 -0500

    mejoras en el ejercicio 1

--- Taller 1/ejercicio1.php
+++ Taller 1/ejercicio1.php
@@ -1,22 +1,24 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <title>Resultado números pares e impares</title>
     <link rel="stylesheet" href="estilos.css">
 </head>
+
 <body>
     <h1>Resultado de los números pares e impares</h1>
 
     <div class="resultado">
-    <?php
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
             $entrada = $_POST["numeros"];
             $numeros = explode(",", $entrada);
 
             foreach ($numeros as $num) {
                 $num = trim($num);
-
                 if (is_numeric($num) && intval($num) == $num) {
                     if ($num % 2 == 0) {
                         echo "$num es número par<br>";
@@ -30,13 +32,13 @@
         } else {
             echo "No se recibieron datos.";
         }
-    ?>
+        ?>
     </div>
 
     <br>
-    <a href="index.html">⬅ Volver al menú del Taller 1</a>
+    <a href="index.html">⬅ Volver a pares e impares</a>
     <br>
     <a href="../">⬅ Volver a la lista de talleres</a>
 </body>
-</html>
 
+</html>
